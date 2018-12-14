8d6e4896354692570523040a6d64f9327152fb14
jakahann <jakahann@student.jyu.fi>
2018-12-14 13:41:29 +0200

    added licensing info to website as modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,18 @@
     <div class="header shadow">
       <p>Aseman junatiedot</p></div>
       <nuxt/>
+      <div>
+      <b-btn class="footer" v-b-modal.modal1>Lisenssi</b-btn>
+
+  <!-- Modal Component -->
+  <b-modal id="modal1" :ok-only="true" title="Lisenssi">
+    <p class="my-1" >
+      Palvelun data on liikenneviraston  <a href="https://rata.digitraffic.fi" target="_blank">Digi-traffic palvelusta</a>.
+      Rajapinta on lisenssioitu <a href="http://creativecommons.org/licenses/by/4.0/" target="_blank"> CC 4.0 BY</a> -lisenssillä
+   </p>
+  </b-modal>
+</div>
+
   </div>
 </template>
 
@@ -25,11 +37,17 @@
   margin-top: 50px;
 }
 
+.footer {
+  background-color: gray;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+
 @media screen and (max-width: 500px) {
   .container {
     margin-left: 2px;
   }
-
 }
 
 
